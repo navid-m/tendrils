33426e980f59669a9b4732118d8efd266bc0f9bf
Navid M <navidmomtahen@gmail.com>
2025-11-10 23:33:00 +0000

    amend

--- source/package.d
+++ source/package.d
@@ -7,9 +7,6 @@ unittest
 	auto x = [123];
 	x ~= 200;
 
-	assert(x.length == 2);
-	assert(x[0] == 123);
-	assert(x[1] == 200);
 	printInt(cast(int)x.length);
 	println("\nHello, world");
 	print("Testing ");
