2e5cf7d31ceb84330572304ba2dd64a1b00c09ef
Navid M <navidmomtahen@gmail.com>
2025-11-10 23:32:50 +0000

    amend

--- source/package.d
+++ source/package.d
@@ -4,7 +4,14 @@ public import tendrils.minstdio;
 
 unittest
 {
-	println("Hello, world");
+	auto x = [123];
+	x ~= 200;
+
+	assert(x.length == 2);
+	assert(x[0] == 123);
+	assert(x[1] == 200);
+	printInt(cast(int)x.length);
+	println("\nHello, world");
 	print("Testing ");
 	println("print()");
 	printInt(42);
